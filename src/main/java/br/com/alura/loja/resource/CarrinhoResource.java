abca9606dbc91d41f348e0a82b347f4c21882d9d
Leandro Luiz Pradela <llpradela@gmail.com>
2021-04-13 20:21:25 -0300

    Commit final

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -1,34 +1,81 @@
 package br.com.alura.loja.resource;
 
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
 
-	
+	/**
+	 * método serializado via JAXB
+	 * @param id
+	 * @return
+	 */
 	@Path("{id}")
-	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	public String busca(@PathParam("id") long id) {
+	@GET // GET é idempotete (não importa quantas vezes chama, não alterar nada no
+			// servidor (quando o GET é feito sem gambiarra))
+	@Produces(MediaType.APPLICATION_XML) // produces significa que retorna algo
+	public Carrinho busca(@PathParam("id") long id) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
-		return carrinho.toXml();
+		return carrinho;
+	}
+
+	/**
+	 * método serializado via JAXB
+	 * @param carrinho
+	 * @return Response
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_XML) // consume significa recebe algo
+	public Response adiciona(Carrinho carrinho) {
+		new CarrinhoDAO().adiciona(carrinho);
+		URI uri = URI.create("/carrinhos/" + carrinho.getId());
+		return Response.created(uri).build();
+	}
+
+	/**
+	 * método serializado via JAXB
+	 * @param id
+	 * @param produtoId
+	 * @return Response
+	 */
+	@Path("{id}/produtos/{produtoId }")
+	@DELETE
+	public Response removeProduto(@PathParam("id") long id, @PathParam("produtoId") long produtoId) {
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		carrinho.remove(produtoId);
+		return Response.ok().build();
 	}
 	
-	@POST
-	@Produces(MediaType.APPLICATION_XML)
-	public String adiciona(String conteudo) {
-		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
-		new CarrinhoDAO().adiciona(carrinho);
-		return "<status>sucesso</status>";
+	/**
+	 * método serializado via JAXB
+	 * @param produto
+	 * @param id
+	 * @param produtoId
+	 * @return Response
+	 */
+	@Path("{id}/produtos/{produtoId }")
+	@PUT
+	public Response alteraProduto(Produto produto, 
+			@PathParam("id") long id, @PathParam("produtoId") long produtoId) {
+		Carrinho carrinho = new CarrinhoDAO().busca(id); 
+		carrinho.troca(produto);
+		return Response.ok().build();
 	}
 }
--- src/test/java/br/com/alura/loja/ClientTest.java
+++ src/test/java/br/com/alura/loja/ClientTest.java
@@ -2,9 +2,14 @@ package br.com.alura.loja;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,15 +18,25 @@ import org.junit.Test;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 public class ClientTest {
 	
 	private HttpServer server;
+	private WebTarget target;
+	private Client client;
 
 	//rodar servidor antes
 	@Before
 	public void startaServidor() {
 		server = Servidor.inicializaServidor();
+		//configurar o servidor
+		ClientConfig config = new ClientConfig();
+		//implementar um log
+		config.register(new LoggingFilter());
+		client = ClientBuilder.newClient(config); //insere o config no servidor
+		
+		this.target = client.target("http://localhost:8080");
 	}
 	
 	@After
@@ -32,21 +47,37 @@ public class ClientTest {
 	
 	@Test
 	public void testeConexaoFunciona () {
-		
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
-		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
+		//testar uma URL qualquer
+		WebTarget targetMockIo = client.target("http://www.mocky.io");
+		String conteudo = targetMockIo.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
 		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
 		System.out.println("Primeiro teste de consumo");
 	}
 	
 	@Test
 	public void testTrazerCarrinhoEsperado() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/carrinhos/1").request().get(String.class);
-		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		//testar o GET
+		Carrinho carrinho = target.path("/carrinhos/1").request().get(Carrinho.class);
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
-		System.out.println("Já rodou o teste de WS");
+	}
+	
+	@Test
+	public void testeQueSuportaNovosCarrinhos() {
+		//montar um carrinho e disparar o POST
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314, "Microfone", 37, 1));
+		carrinho.setRua("Rua Vergueiro 1001");
+		carrinho.setCidade("Sao Paulo");
+		Entity<Carrinho> entity = Entity.entity(carrinho, MediaType.APPLICATION_XML);
+		
+		//verificar se retornou o 201 do POST
+		Response response = target.path("/carrinhos").request().post(entity);
+		Assert.assertEquals(201, response.getStatus());
+		
+		//pegar o header location do POST
+		String location = response.getHeaderString("Location");
+		//acessar a URL do location via GET
+		Carrinho carrinhoResponse = client.target(location).request().get(Carrinho.class);
+		Assert.assertEquals("Microfone", carrinhoResponse.getProdutos().get(0).getNome());
 	}
 }
